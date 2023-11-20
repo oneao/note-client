bfc899c491f386a127c958c6b53e3952aa37bada
oneao <hi_itao@163.com>
2023-11-20 21:43:32 +0800

    push

--- src/test/java/cn/oneao/noteclient/NoteClientApplicationTests.java
+++ src/test/java/cn/oneao/noteclient/NoteClientApplicationTests.java
@@ -7,6 +7,11 @@ import cn.oneao.noteclient.pojo.vo.NoteShareAllVO;
 import cn.oneao.noteclient.utils.GlobalObjectUtils.UserContext;
 import cn.oneao.noteclient.utils.HttpClientUtil;
 import cn.oneao.noteclient.utils.RedisCache;
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.indices.CreateIndexResponse;
+import co.elastic.clients.elasticsearch.indices.DeleteIndexResponse;
+import co.elastic.clients.elasticsearch.indices.GetIndexResponse;
+import co.elastic.clients.transport.endpoints.BooleanResponse;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson2.JSONObject;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +19,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
@@ -22,8 +28,24 @@ import java.util.List;
 @Slf4j
 class NoteClientApplicationTests {
     @Autowired
-    private NoteShareMapper noteShareMapper;
+    private ElasticsearchClient client;
+
     @Test
-    void contextLoads() {
+    public void createTest() throws IOException {
+        CreateIndexResponse indexResponse = client.indices().create(c -> c.index("user"));
+    }
+    @Test
+    public void queryTest() throws IOException {
+        GetIndexResponse getIndexResponse = client.indices().get(i -> i.index("user"));
+    }
+    @Test
+    public void existsTest() throws IOException {
+        BooleanResponse booleanResponse = client.indices().exists(e -> e.index("user"));
+        System.out.println(booleanResponse.value());
+    }
+    @Test
+    public void deleteTest() throws IOException {
+        DeleteIndexResponse deleteIndexResponse = client.indices().delete(d -> d.index("user"));
+        System.out.println(deleteIndexResponse.acknowledged());
     }
 }
